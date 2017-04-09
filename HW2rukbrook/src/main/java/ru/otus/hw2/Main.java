35f76918adafa36e04cbf7ec293e5dba218295eb
Mark Brook <rukbrook@gmail.com>
2017-04-09 23:33:58 +0300

    Implementation through BeanUtils

--- HW2rukbrook/src/main/java/ru/otus/hw2/Main.java
+++ HW2rukbrook/src/main/java/ru/otus/hw2/Main.java
@@ -1,13 +1,14 @@
 package ru.otus.hw2;
 
 import java.lang.management.ManagementFactory;
+import java.lang.reflect.InvocationTargetException;
 
 /**
  * Created by rukbr on 08.04.2017.
  */
 //VM options -Xmx512m -Xms512m
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
         System.out.println("Starting pod: " + ManagementFactory.getRuntimeMXBean().getName());
         Runtime runtime = Runtime.getRuntime();
         MeasuringStand<String> test = new MeasuringStand<>(runtime, new String(""));
--- HW2rukbrook/src/main/java/ru/otus/hw2/MeasuringStand.java
+++ HW2rukbrook/src/main/java/ru/otus/hw2/MeasuringStand.java
@@ -1,7 +1,8 @@
 package ru.otus.hw2;
 
+import org.apache.commons.beanutils.BeanUtils;
 
-import com.rits.cloning.Cloner;
+import java.lang.reflect.InvocationTargetException;
 
 /**
  * Created by rukbr on 09.04.2017.
@@ -9,15 +10,11 @@ import com.rits.cloning.Cloner;
 public class MeasuringStand<T> {
     private Runtime runtime;
     private Object[] array;
-    private T originalCopy;
-    Cloner cloner;
     private int size = 10*1024*1024;
     private long averageSize;
 
-    public MeasuringStand(Runtime runtime, T originalCopy) throws InterruptedException {
+    public MeasuringStand(Runtime runtime, T originalCopy) throws InterruptedException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         this.runtime = runtime;
-        this.originalCopy = originalCopy;
-        this.cloner = new Cloner();
         calcAverageSize(originalCopy);
     }
 
@@ -25,27 +22,29 @@ public class MeasuringStand<T> {
         return averageSize;
     }
 
-    private void calcAverageSize(T originalCopy) throws InterruptedException {
+    private void calcAverageSize(T originalCopy) throws InterruptedException, NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
         int result = 0;
         for(int i=0; i<5; i++)
         {
             result+= calcSize(originalCopy);
+            System.out.println("i= " + i);
         }
         averageSize = result/5;
     }
 
-    private long calcSize(T originalCopy) throws InterruptedException {
+    private long calcSize(T originalCopy) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException, InterruptedException {
         array = new Object[size];
         for(int i = 0; i < size; i++)
         {
-            array[i] = cloner.deepClone(originalCopy);
+            T tmp = (T) BeanUtils.cloneBean(originalCopy);
+            array[i] = tmp;
         }
-        Thread.sleep(10*1000);
+        Thread.sleep(1000);
         long start = runtime.freeMemory();
         array = null;
         System.gc();
+        Thread.sleep(10000);
         long end = runtime.freeMemory();
-
         return (end-start)/size;
     }
 }
